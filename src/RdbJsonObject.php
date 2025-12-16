7187cc659453d95b50634d4cd61ab4b08d4d635e
Gwilyn <gwilyn@karmabunny.com.au>
2025-12-16 13:54:51 +1100

    Update docs for RdbJsonObject.

--- src/RdbJsonObject.php
+++ src/RdbJsonObject.php
@@ -8,8 +8,15 @@ use karmabunny\interfaces\JsonDeserializable;
 /**
  * Implement this interface to create a JSON object that can be stored by Rdb.
  *
- * @see Rdb::setJsonObject()
- * @see Rdb::getJsonObject()
+ * This is a convenience interface that combines JsonSerializable and JsonDeserializable.
+ *
+ * Use this for object drivers:
+ * - HashObjectDriver
+ * - JsonObjectDriver
+ * - MsgPackObjectDriver
+ *
+ * @see Rdb::setObject()
+ * @see Rdb::getObject()
  */
 interface RdbJsonObject extends JsonSerializable, JsonDeserializable
 {
